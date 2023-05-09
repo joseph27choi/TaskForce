572dc1fbbc628031527d02588cb39e9fb8c3aca6
Joseph Choi <choi.joseph0302@gmail.com>
2023-05-09 19:28:41 -0400

    made homepage default directory

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Pomodoro from './pages/Pomodoro';
 function App() {
   return (
     <Routes>
-      <Route path='/home' element={<HomePage />} />
+      <Route path='/' element={<HomePage />} />
       <Route path='/loading' element={<LoadingPage />} />
       <Route path='/pomodoro' element={<Pomodoro />} />
     </Routes>
